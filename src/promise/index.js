c01390227f2c74385047b94657d50d0d1b6074b6
Cristhian Castro <criscastrog10@gmail.com>
2023-10-18 15:50:48 -0500

    [ADD] Promise

--- src/promise/index.js
+++ src/promise/index.js
@@ -2,4 +2,22 @@ const myPromise = new Promise((resolve, reject) => {
     resolve('hey');
 });
 
-const 
+
+// Promise 2
+const cows = 9;
+
+const countCows = new Promise((resolve, reject) =>{
+    if (cows > 10){
+        resolve('Correcto');
+    } else {
+        reject('No existen');
+    }
+});
+
+countCows.then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log(error);
+}).finally(() => {
+    console.log('END')
+})
