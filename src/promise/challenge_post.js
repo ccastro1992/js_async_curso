5b408e6eafbf96d5834ce404e8ad49f8bd6b05bd
Cristhian Castro <criscastrog10@gmail.com>
2023-10-18 17:22:45 -0500

    [ADD] FetchData Post

---
+++ src/promise/challenge_post.js
@@ -0,0 +1,36 @@
+import fetch from "node-fetch";
+const API = 'https://api.escuelajs.co/api/v1';
+
+function postData(urlApi, data) {
+    const response = fetch(urlApi, {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers: {
+            'COntent-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+    return response
+}
+
+// [POST] https://api.escuelajs.co/api/v1/products/
+// {
+//   "title": "New Product",
+//   "price": 10,
+//   "description": "A description",
+//   "categoryId": 1,
+//   "images": ["https://placeimg.com/640/480/any"]
+// }
+
+const data = {
+    "title": "New Product Cris",
+    "price": 15.80,
+    "description": "A description",
+    "categoryId": 1,
+    "images": ["https://placeimg.com/640/480/any"]
+}
+
+postData(`${API}/products`, data)
+    .then(response => response.json())
+    .then(data => console.log(data))
