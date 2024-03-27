d2b364145c69c79c0e3f338d9546b73bf9a912eb
Cristhian Castro <c.castro@protelcotelsa.com>
2024-03-27 16:35:55 -0500

    [FIX] Ultimos cambios de async-await challege

--- src/async_await/challenge.js
+++ src/async_await/challenge.js
@@ -12,11 +12,10 @@ const myFunc = async (urlApi) => {
     try {
         const products = await fetchData(`${API}/products`);
         const product = await fetchData(`${API}/products/${products[0].id}`);
-        const category = await fetchData(`${API}/categories/${product?.category.id}`)
 
         // console.log(products);
         console.log(product);
-        console.log(category);
+        console.log('error');
     } catch (error) {
         console.error(error);
     }
